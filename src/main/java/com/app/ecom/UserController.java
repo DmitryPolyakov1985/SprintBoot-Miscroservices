075183c385b58b65f2f0a675daa4dc3cd0f0b1aa
Dmitry <dmitry@smilecdr.com>
2025-11-16 00:44:25 -0700

    user controllers and service congigured

---
+++ src/main/java/com/app/ecom/UserController.java
@@ -0,0 +1,46 @@
+package com.app.ecom;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+//    @RequestMapping(value = "/api/users", method = RequestMethod.GET)
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(userService.fetchAllUsers(),HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        return userService.fetchUser(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createUser(@RequestBody User user) {
+        userService.addUser(user);
+        return new ResponseEntity<String>("User added successfully", HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateUser(@PathVariable Long id, @RequestBody User updatedUser) {
+        boolean updated = userService.updateUser(id, updatedUser);
+        if (updated) {
+            return new ResponseEntity<>("User updated successfully", HttpStatus.OK);
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
